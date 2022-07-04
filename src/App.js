1a9cdf030ec6d91942d7d4fdfb657db719be98df
Bernie Chen <bernie0716@gmail.com>
2022-07-04 17:58:08 +0800

    TopBarStyling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import OutlinedCard from "./components/Card/card.jsx";
 import { AppBar, Typography, Button} from "@mui/material";
 import {BrowserRouter, Routes, Route, Link, useNavigate} from "react-router-dom";
  
-import Projects from "./pages/Projects.jsx"
+import Projects from "./pages/projectpages/Projects.jsx"
 import TopBar from "./components/TopBar/TopBar.jsx"
 function App() {
 
@@ -15,7 +15,7 @@ function App() {
         <header className="header">
           <TopBar/>
         </header>
-        <div>
+        <div className = "body">
           
           <Routes>
             <Route path="/projects" element = {<Projects/>}/>
--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,39 +1,57 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 
-import { Button} from "@mui/material";
-import {useNavigate} from "react-router-dom";
-import "./TopBar.css"
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import TopBarCss from "./TopBar.module.css";
 const TopBar = () => {
   let navigate = useNavigate();
   const [selectedTab, setSelected] = useState("projects");
- 
+
   return (
-    <div className = "topbar">
-      <h2 className="title">BC Portfolio</h2>
+    <div className={TopBarCss.topbar}>
+      <h2 className={TopBarCss.title}>BC Coding Portfolio</h2>
 
-      {/* <Link to ="projects"> */}
-      <div   
-        
-        className = {"projectsButton" + (selectedTab === "projects" ? "Selected":"Unselected")}
-        onClick={() => {
-          navigate("/projects");
-          setSelected("projects");
-        }}
-
-      >
-        <div className="projects">Projects</div>
-      </div >
-      <Button
-        
-        onClick={() => {
-          navigate("/experience");
-          setSelected("experience");
-        }}
-
-      >
-        <div className="projects">Experience</div>
-      </Button>
-      {/* </Link> */}
+      <div className={TopBarCss.rightSide}>
+        <div
+          className={
+            selectedTab === "projects"
+              ? TopBarCss.ButtonSelected
+              : TopBarCss.ButtonUnselected
+          }
+          onClick={() => {
+            navigate("/projects");
+            setSelected("projects");
+          }}
+        >
+          <div>Projects</div>
+        </div>
+        <div
+          className={
+            selectedTab === "experience"
+              ? TopBarCss.ButtonSelected
+              : TopBarCss.ButtonUnselected
+          }
+          onClick={() => {
+            navigate("/experience");
+            setSelected("experience");
+          }}
+        >
+          <div>Coding Experience</div>
+        </div>
+        <div
+          className={
+            selectedTab === "aboutme"
+              ? TopBarCss.ButtonSelected
+              : TopBarCss.ButtonUnselected
+          }
+          onClick={() => {
+            navigate("/aboutme");
+            setSelected("aboutme");
+          }}
+        >
+          <div>About Me</div>
+        </div>
+      </div>
     </div>
   );
 };
