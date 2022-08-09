3b87ce5512a3df6942b4128b4f4ab3109bddd667
Sophia Hsieh <shensophia66@gmail.com>
2022-08-09 21:46:54 +0800

    fix gh-page problem

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 import { AppBar, Typography, Button } from "@mui/material";
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route,
   Link,
@@ -13,17 +13,16 @@ import {
 import Projects from "./pages/projectpages/Projects.jsx";
 import TopBar from "./components/TopBar/TopBar.jsx";
 import Aboutme from "./pages/aboutme/Aboutme.jsx";
-import Experience from "./pages/experience/Experience.jsx"
-import Landing from "./pages/landingpage/Landing.jsx"
-import End from "./components/footer/End.jsx"
+import Experience from "./pages/experience/Experience.jsx";
+import Landing from "./pages/landingpage/Landing.jsx";
+import End from "./components/footer/End.jsx";
 
 function App() {
   // mac screen 1512 x 982
   return (
-    
     <div className="App">
-      <BrowserRouter>
-        <header id = "header" className="header">
+      <HashRouter basename={process.env.PUBLIC_URL}>
+        <header id="header" className="header">
           <TopBar />
         </header>
         {/* <script>
@@ -31,22 +30,19 @@ function App() {
         </script> */}
         <div className="body">
           <Routes>
-            <Route path="/home" element={<Landing/>}/>
-            <Route path="/experience" element={<Experience/>}/>
-            <Route path="/aboutme" element={<Aboutme/>} />
+            <Route path="/home" element={<Landing />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/aboutme" element={<Aboutme />} />
             <Route path="/project">
-              <Route path=":pageTitle" element={<Projects/>}/>
+              <Route path=":pageTitle" element={<Projects />} />
             </Route>
-            <Route path="*" element={<Navigate to = "/home" replace />}/>
-            
+            <Route path="*" element={<Navigate to="/home" replace />} />
           </Routes>
         </div>
         <footer className="footer">
-          <End/>
+          <End />
         </footer>
-      </BrowserRouter>
-
-
+      </HashRouter>
     </div>
   );
 }
