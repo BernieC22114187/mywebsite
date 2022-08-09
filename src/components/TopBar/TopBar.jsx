1f888e6cf854a7357697dace8bfcc363f926cf89
Sophia Hsieh <shensophia66@gmail.com>
2022-08-09 22:23:41 +0800

    fix hashroute href

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -6,71 +6,67 @@ import TopBarCss from "./TopBar.module.css";
 
 const TopBar = () => {
   let navigate = useNavigate();
-  const url = window.location.href.split("/")[3];
-  
+  const url = window.location.href.split("/")[4];
   const [selectedTab, setSelected] = useState(url);
-  
-  const refreshPage = () => {
-    window.location.reload()
-    
 
-  }
-  
-  
-  
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
   return (
     <div className={TopBarCss.topbar}>
-      
       <button
-          className={
-            selectedTab === "home"
-              ? TopBarCss.homeSelected
-              : TopBarCss.homeUnselected
-          }
-          onClick={() => {
-            
-            navigate("/home");
-            setSelected("home");
-            refreshPage();
-            
-          }}
-        >
-          <h2 className={TopBarCss.title}>BC Coding Portfolio</h2>
-        </button>
-      
+        className={
+          selectedTab === "home"
+            ? TopBarCss.homeSelected
+            : TopBarCss.homeUnselected
+        }
+        onClick={() => {
+          navigate("/home");
+          setSelected("home");
+          refreshPage();
+        }}
+      >
+        <h2 className={TopBarCss.title}>BC Coding Portfolio</h2>
+      </button>
 
       <div className={TopBarCss.rightSide}>
-        <div className = {TopBarCss.dropdowncontainer}>
+        <div className={TopBarCss.dropdowncontainer}>
           <div className={TopBarCss.dropdown}>
             <button
               className={
-                
                 selectedTab === "project"
-                ? TopBarCss.dropbtnSelected
-                : TopBarCss.dropbtn
+                  ? TopBarCss.dropbtnSelected
+                  : TopBarCss.dropbtn
               }
-              
             >
               Projects
             </button>
 
             <div className={TopBarCss.dropdownContent}>
-              <button  className={TopBarCss.dropdownbtn} onClick={() => {
-                navigate("/project/aiProjects");
-                setSelected("project");
-                refreshPage();
+              <button
+                className={TopBarCss.dropdownbtn}
+                onClick={() => {
+                  navigate("/project/aiProjects");
+                  setSelected("project");
+                  refreshPage();
                 }}
-                >Honors AI Projects</button>
-              <button className={TopBarCss.dropdownbtn} onClick={() => {
-                navigate("/project/modernTopics");
-                setSelected("project");
-                refreshPage();
+              >
+                Honors AI Projects
+              </button>
+              <button
+                className={TopBarCss.dropdownbtn}
+                onClick={() => {
+                  navigate("/project/modernTopics");
+                  setSelected("project");
+                  refreshPage();
                 }}
-                >Honors Modern Topics in CS Projects</button>
+              >
+                Honors Modern Topics in CS Projects
+              </button>
             </div>
           </div>
         </div>
-        
 
         <button
           className={
